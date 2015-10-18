c57898eaf0872208c099a2f270cb69dbe2c08fc1
yanglikun <hackerster@qq.com>
2015-10-18 23:03:51 +0800

    init

---
+++ popup.js
@@ -0,0 +1,44 @@
+localStorage.setItem("proxyStatus",localStorage.getItem("proxyStatus")==null?0:localStorage.getItem("proxyStatus"))
+$(function () {
+    $("#enProxyBtn").click(function () {
+        enProxy();
+    })
+    $("#disProxyBtn").click(function () {
+        disProxy();
+    })
+    showStatusFun();
+})
+var proxyConfig = {
+    mode: "fixed_servers",
+    rules: {
+        singleProxy: {
+            scheme: "http",
+            host: "192.168.147.89",
+            port: 8123
+        },
+        bypassList: ["ip138.com"]
+    }
+};
+
+var sytemConfig = {
+    mode: "system"
+};
+var enProxy = function () {
+    chrome.proxy.settings.set(
+        {value: proxyConfig, scope: 'regular'},
+        function () {
+        });
+    localStorage.setItem("proxyStatus",1)
+    showStatusFun();
+}
+var disProxy = function () {
+    chrome.proxy.settings.set(
+        {value: sytemConfig, scope: 'regular'},
+        function () {
+        });
+    localStorage.setItem("proxyStatus",0)
+    showStatusFun();
+}
+var showStatusFun=function(){
+    $("#showStatus").text(localStorage.getItem("proxyStatus")==0?"禁用":"启用")
+}
